e8cafd7f3035a67cd2f0b8edecb9a48c47b67769
pafech <patrick.fehr@fbc.ch>
2026-08-19 19:40:17 +0200

    ui: use mutableIntStateOf for btFlashTrigger to avoid Int autoboxing

--- app/src/main/java/ch/fbc/krakenbridge/ui/MainScreen.kt
+++ app/src/main/java/ch/fbc/krakenbridge/ui/MainScreen.kt
@@ -21,7 +21,7 @@ import androidx.compose.animation.core.Animatable
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -66,7 +66,7 @@ fun MainScreen(
         ConnectionStatus.Error -> ConnectionPhase.Idle
     }
     val readyToConnect = cameraReady && bluetoothEnabled
-    var btFlashTrigger by remember { mutableStateOf(0) }
+    var btFlashTrigger by remember { mutableIntStateOf(0) }
 
     Box(modifier = Modifier.fillMaxSize()) {
 
